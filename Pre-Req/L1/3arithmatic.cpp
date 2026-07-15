6c722203bd3317867227ce9ddebed20a04191039
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-15 15:00:44 +0500

    Add output statements for sum, difference, product, and quotient in arithmetic program

--- Pre-Req/L1/3arithmatic.cpp
+++ Pre-Req/L1/3arithmatic.cpp
@@ -7,6 +7,11 @@ int main(){
     int a,b;
     cout<< "enter two number to print their sum , difference, product and quotient: ";
     cin >> a >> b;
+    
+    cout << "the sum of " << a << " and " << b << " is:  "<< a+b << endl;
+    cout << "the difference of " << a << " and " << b << " is:  "<< a-b << endl;
+    cout << "the product of " << a << " and " << b << " is:  "<< a*b << endl;
+    cout << "the quotient of " << a << " and " << b << " is:  "<< a/b << endl;  
     return 0;
 
 }
