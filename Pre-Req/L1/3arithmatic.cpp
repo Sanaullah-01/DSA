eef70f89215f7210318ee9be4c8cf127c4771c79
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-15 14:52:31 +0500

    Add program to read two numbers and print their sum, difference, product, and quotient

---
+++ Pre-Req/L1/3arithmatic.cpp
@@ -0,0 +1,12 @@
+//Write a program to read two numbers and print their sum, difference, product and 
+// quotient.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<< "enter two number to print their sum , difference, product and quotient: ";
+    cin >> a >> b;
+    return 0;
+
+}
