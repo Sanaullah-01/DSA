37d4232ba9226c39908e579a79ef4aa60bda124a
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-15 13:54:49 +0500

    Add program to calculate the sum of two numbers and include executable

---
+++ Pre-Req/L1/2sum.cpp
@@ -0,0 +1,11 @@
+//Write a program to read two numbers and print their sum. 
+
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"Enter two numbers to print their sum: ";
+    cin >> a >> b;
+    cout << "the sum of " << a << " and " << b << " is:  "<< a+b;
+    return 0;
+}
